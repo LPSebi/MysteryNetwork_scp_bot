8c0ca24d0d6d57404d0c3b50a334c1e1861ce30f
DevSebi <52174534+LPSebi@users.noreply.github.com>
2023-01-19 16:26:56 +0100

    wrong indentation I hate python

--- src/bot.py
+++ src/bot.py
@@ -373,7 +373,7 @@ async def on_member_update(before: discord.Member, after: discord.Member):
             highest_role = discord.utils.find(lambda role: role in roles,
                                               reversed(member.roles))
             roleembed.add_field(name=f"{member.name}#{member.discriminator}", value=member.mention, inline=False) if highest_role == role else None
-            await teamlist_channel.send(embed=roleembed)
+        await teamlist_channel.send(embed=roleembed)
 
 
 if __name__ == '__main__':
